acb8b34925e127290e46356436da42f5682c40f9
Nicolas Porreca <nicolas.porreca@gmail.com>
2019-02-25 19:49:42 +0100

    more fixes on calc. should be tested in limit cases

--- js/main.js
+++ js/main.js
@@ -21,27 +21,36 @@ setInterval(updateClock, 1000);
     
 /*Calc*/
 var display = document.getElementById('risultato');
-var displayString = '';
-var dotUsed = false;
+var displayString, dotUsed, nonZero, lastIsEqual;
 resetCalc();
 
 function resetCalc() {
   displayString = '0';
   display.value = '0';
+  dotUsed = false;
+  nonZero = false;
+  lastIsEqual = true;
 }
  
 function addCalc(char) {
+  //console.log(displayString)
   var firstChar = displayString[0];
   var lastChar = displayString[displayString.length -1];
+  
   switch (char) {
+ 
     case '.':
       if (dotUsed) {
         return
       } else {
+        if (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/' || displayString === '') displayString = displayString + '0';
         dotUsed = true;
-        if (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/') displayString = displayString + '0';
+        nonZero = true;
       }
+      lastIsEqual ? displayString = '0' + char : displayString += char;
+      
     break;
+
     case '+':
     case '-':
     case '*':
@@ -50,15 +59,39 @@ function addCalc(char) {
       if (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/') {
         displayString = displayString.substring(0, (displayString.length -1));
       }
+      displayString += char;
       dotUsed = false;
+      nonZero = false;
+    break;
+
+    case '0':
+      if (!nonZero) return;
+      if (lastIsEqual) {
+        displayString = char;
+      } else {
+        displayString += char;
+      }
+    break;
+
+    default:
+      if (displayString === '0') displayString = '';
+      if (lastIsEqual) {
+        displayString = char;
+      } else {
+        displayString += char;
+      }
+      nonZero = true;
     break;
   }
-  (displayString === '0' || displayString === '-0') ? displayString = char : displayString += char;
+  
+  lastIsEqual = false;
   if (firstChar === '+' || firstChar === '*' || firstChar === '/') displayString = displayString.substring(1, (displayString.length));
   display.value = displayString;
+  //console.log(displayString)
 }
 
 function equalsCalc(){
+  lastIsEqual = true;
   var lastChar = displayString[displayString.length -1];
   if (lastChar === '.') displayString += '0';
   if (lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/') {
@@ -66,7 +99,7 @@ function equalsCalc(){
   }
   displayString = "" + eval(displayString);
   display.value = displayString;
-  displayString = '';
+  //displayString = '';
 }
 
 function changeCalc(){
