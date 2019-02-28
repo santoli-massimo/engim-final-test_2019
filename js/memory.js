b789dc3f437fa73384d1b9cd0d782ace9a56c878
Flavio Cherchi <flavio.cherchi@gmail.com>
2019-02-28 18:33:57 +0100

    Memory test, works in progress

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,40 @@
+var gabbia = [
+  "Topo",
+  "Bue",
+  "Tigre",
+  "Coniglio",
+  "Drago",
+  "Serpente",
+  "Cavallo",
+  "Capra",
+  "Scimmia",
+  "Gallo",
+  "Cane",
+  "Maiale"
+];
 
-var gabbia = ['Topo', 'Bue', 'Tigre', 'Coniglio', 'Drago', 'Serpente', 'Cavallo',
-    'Capra', 'Scimmia', 'Gallo', 'Cane', 'Maiale']
- 
-function memoria() {            //usare un do while
-        var res2 = "";
-    for (i = 0; i < 8; i++) {
+function memoria() {
 
-        var res = gabbia[Math.floor(Math.random() * 8)];
+  var a = 0;
+  for (i = 0; i < 16; i++) {
+    a++;
+    
+    var res = gabbia[Math.floor(Math.random() * 8)];
 
-
-        if (res === res2) {
-            var result = "img/zoo/" + gabbia[Math.floor(Math.random() * 8)] + ".jpg";
-        }
-        console.log(result);
-    }
+    var icon = "icon" + a;
+    var res1 =
+      "<img src=img/zoo/" +
+      gabbia[Math.floor(Math.random() * 8)] +
+      '.jpg class="icon">';
+    var res2 = res1;
+    console.log("res1: "+res1);
+    console.log("res2: "+res2);
+    document.getElementById(icon).innerHTML = res1;
+    document.getElementById(icon).innerHTML = res1;
+  }
 }
-
 memoria();
+
+function restart() {
+  document.getElementById("nuova").innerHTML = memoria();
+}
