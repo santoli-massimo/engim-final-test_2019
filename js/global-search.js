266670ddb9244aa30dbc66030787b85ee38b25f4
Nicolas Porreca <nicolas.porreca@gmail.com>
2019-02-28 19:58:50 +0100

    fixed issue on navlink highligh when using global search, closed a div in html

--- js/global-search.js
+++ js/global-search.js
@@ -3,10 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const searchbar = document.getElementById('js-gs-text');
     const checkbox = document.getElementById('js-gs-check');
-    let occurrences = [];
-    let matches = [];
-    let allChildren = [];
-    let searchRegex;
     const foundRegex = /(<span class="gs-found">)(.*)(<\/span>)/;
     const bacon = document.getElementById('txtBacon').innerHTML;
 
@@ -25,7 +21,10 @@ document.addEventListener('DOMContentLoaded', () => {
         globalSearch(checkbox.checked, searchbar.value);
     });
 
+    /* Clear */
     const clearSearch = () => {
+        let occurrences = [];
+
         occurrences = Array.from(document.querySelectorAll('.gs-found'));
 
         occurrences.forEach((match) => {
@@ -35,48 +34,56 @@ document.addEventListener('DOMContentLoaded', () => {
 
     }
 
+    /* RegExp escape */
     const escapeRegExp = (string)  => {
         return string.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
     }
 
+    /* Actual Search */
     const globalSearch = (caseSensitive, searchedText) => {
+        let searchRegex;
+        let allChildren = [];
+        let matches = [];
+
+        /* Clear first */
         clearLocalSearch();
         clearSearch();
 
-        // reset link dot
+        // Reset link dots
         Array.from(document.querySelectorAll('.gs-occurrences')).forEach((link) => {
             link.classList.remove('gs-occurrences');
         });
 
+        /* Search empty */
         if (!searchedText) return;
         else searchedText = escapeRegExp(searchedText);
 
+        /* Create RegEx based on case sensitivity */
         //console.log(`String searched: ${searchedText} (${caseSensitive ? 'case sensitive' : 'case insensitive'})`);
         caseSensitive ? searchRegex = new RegExp(`(${searchedText})`, 'g') : searchRegex = new RegExp(`(${searchedText})`, 'gi');
         //console.log(searchRegex)
 
+        /* Search on all elements inside pages */
         allChildren = Array.from(document.querySelectorAll('#pages *'));
 
+        /* Add highlight on occurrences and nav links */
         allChildren.forEach((child) => {
             matches = child.innerHTML.match(searchRegex);
             if ((matches) && (child.innerHTML === child.textContent)) {
                 child.innerHTML = child.innerHTML.replace(searchRegex, `<span class="gs-found">${'$1'}</span>`);
+                // Add link dot
+                let linkId = (child.closest('#pages > div').id);
+                let link = document.querySelector(`[data-page=${linkId}`);
+                if (!link.classList.contains('gs-occurrences')) link.classList.add('gs-occurrences');
                 matches = [];
             }
         });
         allChildren = [];
-
-        /* Highlight links */
-        pages = Array.from(document.querySelectorAll('[data-page*="page"]'));
-        pages.forEach((page) => {
-            if (page.innerHTML.match(searchRegex)) {
-                page.classList.add('gs-occurrences');
-            }  
-        });
         
     }
 
     /* FIXES FOR LOCAL SEARCH */
+    /* Used to prevent a double search which could break the HTML due to the different searching algorithms */
     document.getElementById('txtcerca').addEventListener('input', (ev) => {
         searchbar.value = '';
         clearSearch();
