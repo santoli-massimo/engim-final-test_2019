18fc52ec384f1dd57f7e6249d2d162f87c42960f
OlgaAlessia <olga.cercalavoro.com>
2019-02-28 11:22:45 +0100

    Developed Form Aggiungi

--- js/main.js
+++ js/main.js
@@ -125,7 +125,7 @@ mainNav.addEventListener('click', function(e){
   
 })
 
-/*------cerca----------*/
+/*------ Cerca Parole Locale ----------*/
 
 var btCerca = document.getElementById('btcerca');
 document.getElementById('write').innerHTML = " ";
@@ -135,7 +135,7 @@ btCerca.addEventListener('click', function(e){
   
   var txt = document.getElementById('txtBacon').textContent;
   
-  if (parola != "" || parola != NaN){
+  if (parola.trim() != ""){
     
     document.getElementById('write').innerHTML = " ";
     
@@ -176,94 +176,6 @@ btCerca.addEventListener('click', function(e){
     document.getElementById('write').innerHTML = "Immettere parola da cercare!";
   }
 });
-
-/*------form aggiungi----------*/
-
-var aggiungi = document.getElementsByName("Aggiungi");
-var persone=[];
-var i=0; //indice per aggiungere
-aggiungi[0].addEventListener('click', function(){
-  var tab=document.getElementById('lista');
-  var nome=document.forms['persone']['nome'].value;
-  var cognome=document.forms['persone']['cognome'].value;
-  //controllo compilazione campi
-  if (nome == "undefined" || nome == "" || cognome == "undefined" || cognome == "")
-    tab.innerHTML='<p>nome e cognome obbligatori</p>';
-  //eseguo procedura per inserimento
-  else{               
-    t=i;    //indice per controlli 
-    //controllo se già inserito
-    if(i>0){         
-      while(t>0){
-        if(persone[t-1].Nome != nome || persone[t-1].CogNome != cognome)
-        t--;
-        else t=-1;            
-      }
-    } 
-    //eseguo inserimento se persona non ancora presente
-    if(t==0)   
-    {
-      function Persona(nome, cognome) {
-        this.Nome = nome;
-        this.CogNome = cognome;
-      }
-      persone[i] = new Persona(nome, cognome);
-      i++;
-      //creo lista ordinata con nome e cognome persona sotto il form
-      tab.innerHTML='<ol id="0"><li>'+persone[i-1].Nome+" "+persone[i-1].CogNome+'</li></ol>'
-      for(var t=i-1;t>0;t--)
-      {
-        li=document.createElement('li');
-        lin=document.getElementById('0');
-        lin.appendChild(li);
-        li.innerHTML=persone[t-1].Nome+' '+persone[t-1].CogNome;
-      }
-    }
-    // informo che la persona è gia inserita
-    else 
-      tab.innerHTML='<p>dati gia inseriti</p>';
-    
-    document.forms['persone']['nome'].value = "";
-    document.forms['persone']['cognome'].value = "";
-  }
-});
-  
-var btdeleteF = document.getElementsByName("DeleteF");
-btdeleteF[0].addEventListener('click', function(){
-  var list = document.getElementById("lista");
-    var figli = list.childNodes;
-    console.log(figli);
-
-    if (list.hasChildNodes()) {
-      list.removeChild(list.childNodes[0]);
-    }
-});
-
-/*
-var btdelete = document.getElementsByName("Delete");
-btdelete[0].addEventListener('click', function(){
-
-  var tab = document.getElementById('lista');
-  var nome = document.forms['persone']['nome'].value;
-  var cognome = document.forms['persone']['cognome'].value;
-
-  if (nome == "undefined" || nome == "" || cognome == "undefined" || cognome == "")
-    tab.innerHTML='<p>nome e cognome obbligatori</p>';
-    //eseguo ricerca nome e cognome per cancellarlo
-  else{
-    var flag = false;
-    var lista = document.querySelectorAll('lista');
-    for(var i=0; i<persone.length; i++) {
-        if(persone[i].Nome == nome && persone[i].CogNome == cognome){
-          lista.removeChild(lista.childNodes[i]);
-          console.log(lista.removeChild(lista.childNodes[i]));
-          console.log(lista.childNodes[i]);
-        }
-    }
-    if(flag)
-      tab.innerHTML='<p>nome e cognome non corrispondono a quelli immessi precedentemente</p>';
-  }
-});*/ 
   
 /*------ZOO------*/
 
