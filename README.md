5774ae1d4e0dd9931ea217c8ccf502c2079b58fd
Nicolas Porreca <nicolas.porreca@gmail.com>
2019-02-28 13:24:52 +0100

    added link highlight, must fix bugs

--- README.md
+++ README.md
@@ -17,16 +17,16 @@
 
 4. Creare una pagina con un form con campi nome cognome che crea una nuovo oggetto persona, visualizzare lista delle persone sotto al form (se fatto in gruppo fare anche il rimuovi persona)
     > **Daniele:** Aggiungi *done, but it might improve...
+
     > **Olga** Rimuovi *task completed*
 
 5. Creare una pagina con una select con i nomi di animali, al select mostrare la foto dell animale sotto alla select
     > **Mario:** *task completed*
 
 6. Creare una pagina con una calcolatrice
-    > **Flavio:** *graphic completed, task 90%*
+    > **Flavio:** *task completed*
 
-    > **Nicolas:** *la soluzione di Daniele lasciava scoperto un altro bug, ovvero la possibilità di inserire più volte la virgola. Ho rimodificato il tutto, sembra funzionare ma deve essere ulteriormente testato nei casi limite. Ah, ho ingrandito la calcolatrice
-    che stavo diventando cieco!*
+    > **Nicolas:** *stability fixes*
 
 7. Creare una pagina con vari paragrafi al click cambiare colore al pargrafo
     > **???:** *status*
@@ -42,10 +42,8 @@
 - **Per Olga:** ho implementato un fix nella ricerca globale in modo da non avere confitti con quella locale. Per fare ciò devo fare riferimento a due dei tuoi `id`, che sono `txtBacon` e `txtcerca`. Se hai necessità di modificarli, please tell me!
 *Nicolas*
 
-- **Per Nicolas:**  La calcolatrice presenta errori di calcolo (es 8.6*6=51.59999...4)                      e presenta un problema con la virgola, se si fa una operazione in
-                    cui entrambi i valori sono con virgola nelle operazioni successive
-                    non permette più di usare la virgola (sembra con tutti i tipi di 
-                    operazione).Daniele
+- **Per Nicolas:**  La calcolatrice presenta errori di calcolo (es 8.6*6=51.59999...4)                      e presenta un problema con la virgola, se si fa una operazione in cui entrambi i valori sono con virgola nelle operazioni successive
+non permette più di usare la virgola (sembra con tutti i tipi di operazione). *Daniele* `fixed`
 
 - **Per tutti:** ho testato in vari modi la ricerca globale e mi sembra non ci siano bug e non si spacchi niente, ma se doveste trovare qualche bug vi prego di fare uno screenshot e farmelo avere! Più occhi notano molte più cose! Grazie!
 *Nicolas*
--- js/global-search.js
+++ js/global-search.js
@@ -1,64 +1,88 @@
-//console.log('Global search prepped and ready!');
+document.addEventListener('DOMContentLoaded', () => {
+    //console.log('Global search prepped and ready!');
 
-const searchbar = document.getElementById('js-gs-text');
-const checkbox = document.getElementById('js-gs-check');
-let occurrences = [];
-let matches = [];
-let allChildren = [];
-let searchRegex;
-const foundRegex = /(<span class="gs-found">)(.*)(<\/span>)/;
-const bacon = document.getElementById('txtBacon').innerHTML;
+    const searchbar = document.getElementById('js-gs-text');
+    const checkbox = document.getElementById('js-gs-check');
+    let occurrences = [];
+    let matches = [];
+    let allChildren = [];
+    let searchRegex;
+    const foundRegex = /(<span class="gs-found">)(.*)(<\/span>)/;
+    const bacon = document.getElementById('txtBacon').innerHTML;
 
-searchbar.addEventListener('input', (ev) => {
-    globalSearch(checkbox.checked, ev.target.value);
-});
-
-checkbox.addEventListener('input', (ev) => {
-    globalSearch(checkbox.checked, searchbar.value);
-});
-
-const clearSearch = () => {
-    occurrences = Array.from(document.querySelectorAll('.gs-found'));
-
-    occurrences.forEach((match) => {
-        match.outerHTML = match.outerHTML.replace(foundRegex, '$2');
+    /* Prevent form submit on enter */
+    searchbar.addEventListener('keydown', (ev) => {
+        if(ev.keyCode == 13) ev.preventDefault();
+        return false;
     })
-    occurrences = [];
+    
+    /* Triggering search */
+    searchbar.addEventListener('input', (ev) => {
+        globalSearch(checkbox.checked, ev.target.value);
+    });
 
-}
+    checkbox.addEventListener('input', (ev) => {
+        globalSearch(checkbox.checked, searchbar.value);
+    });
 
-const escapeRegExp = (string)  => {
-    return string.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
-}
+    const clearSearch = () => {
+        occurrences = Array.from(document.querySelectorAll('.gs-found'));
 
-const globalSearch = (caseSensitive, searchedText) => {
-    clearLocalSearch();
-    clearSearch();
-    if (!searchedText) return;
-    else searchedText = escapeRegExp(searchedText);
+        occurrences.forEach((match) => {
+            match.outerHTML = match.outerHTML.replace(foundRegex, '$2');
+        })
+        occurrences = [];
 
-    //console.log(`String searched: ${searchedText} (${caseSensitive ? 'case sensitive' : 'case insensitive'})`);
-    caseSensitive ? searchRegex = new RegExp(`(${searchedText})`, 'g') : searchRegex = new RegExp(`(${searchedText})`, 'gi');
-    //console.log(searchRegex)
+    }
 
-    allChildren = Array.from(document.querySelectorAll('#pages *'));
+    const escapeRegExp = (string)  => {
+        return string.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+    }
 
-    allChildren.forEach((child) => {
-        matches = child.innerHTML.match(searchRegex);
-        if ((matches) && (child.innerHTML === child.textContent)) {
-            child.innerHTML = child.innerHTML.replace(searchRegex, `<span class="gs-found">${'$1'}</span>`);
-            matches = [];
-        }
-    })   
-    allChildren = [];
-}
+    const globalSearch = (caseSensitive, searchedText) => {
+        clearLocalSearch();
+        clearSearch();
 
-/* FIXES FOR LOCAL SEARCH */
-document.getElementById('txtcerca').addEventListener('input', (ev) => {
-    searchbar.value = '';
-    clearSearch();
+        // reset link dot
+        Array.from(document.querySelectorAll('.gs-occurrences')).forEach((link) => {
+            link.classList.remove('gs-occurrences');
+        });
+
+        if (!searchedText) return;
+        else searchedText = escapeRegExp(searchedText);
+
+        //console.log(`String searched: ${searchedText} (${caseSensitive ? 'case sensitive' : 'case insensitive'})`);
+        caseSensitive ? searchRegex = new RegExp(`(${searchedText})`, 'g') : searchRegex = new RegExp(`(${searchedText})`, 'gi');
+        //console.log(searchRegex)
+
+        allChildren = Array.from(document.querySelectorAll('#pages *'));
+
+        allChildren.forEach((child) => {
+            matches = child.innerHTML.match(searchRegex);
+            if ((matches) && (child.innerHTML === child.textContent)) {
+                child.innerHTML = child.innerHTML.replace(searchRegex, `<span class="gs-found">${'$1'}</span>`);
+                matches = [];
+            }
+        });
+        allChildren = [];
+
+        /* Highlight links */
+        pages = Array.from(document.querySelectorAll('[data-page*="page"]'));
+        pages.forEach((page) => {
+            if (page.innerHTML.match(searchRegex)) {
+                page.classList.add('gs-occurrences');
+            }  
+        });
+        
+    }
+
+    /* FIXES FOR LOCAL SEARCH */
+    document.getElementById('txtcerca').addEventListener('input', (ev) => {
+        searchbar.value = '';
+        clearSearch();
+    });
+
+    function clearLocalSearch() {
+        document.getElementById('txtBacon').innerHTML = bacon;
+    }
 });
-
-function clearLocalSearch() {
-    document.getElementById('txtBacon').innerHTML = bacon;
-}
