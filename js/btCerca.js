6432680dfa5ca9b69525a03c138214b31a5442f1
OlgaAlessia <olga.cercalavoro@gmail.com>
2019-03-01 19:01:15 +0100

    improvements of btCerca.js

--- js/btCerca.js
+++ js/btCerca.js
@@ -4,35 +4,29 @@ var btCerca = document.getElementById('btcerca');
 document.getElementById('write').innerHTML = " ";
 
 btCerca.addEventListener('click', function(e){
-  var parola = document.getElementById('txtcerca').value;
+  var world = document.getElementById('txtcerca').value;
   
   var txt = document.getElementById('txtBacon').textContent;
   
-  if (parola.trim() != ""){
+  if (world.trim() != "" || !world.includes("<") || !world.includes(">")){
     
     document.getElementById('write').innerHTML = " ";
     
-    if(txt.includes(parola)){
-      if (parola.includes("-")){
-        var parole = txt.split(' ');
-        for(var i=0; i<parole.length; i++){
-          
-          if(parole[i].toLowerCase() == parola){
-            parole[i] = '<span style="background-color: brown; color:white;">' + parole[i] + "</span>";
-          }
-          document.getElementById('txtBacon').innerHTML = parole.join(' ');
-        }
+    if(txt.includes(world)){
+      var parole = txt.split(' ');
+      
+      if (world.includes("-") || world.includes(",") || world.includes(".")){
+
+        stampaConPunt(parole, world);
       } else{
-        
-        var parole = txt.split(' ');
         for(var i=0; i<parole.length; i++){
-          var parolaTemp = parole[i].replace(/(~|`|!|@|#|$|%|^|&|\*|\(|\)|{|}|\[|\]|;|:|\"|'|<|,|\.|>|\?|\/|\\|\||-|_|\+|=)/g, "");
-          if(parolaTemp.trim().toLowerCase() == parola){
-            if(parole[i].toLowerCase() == parola){
+          var parolaTemp = parole[i].replace(/[^A-Za-z0-9]/g, "");
+          if(parolaTemp.trim().toLowerCase() == world){
+            if(parole[i].toLowerCase() == world){
               parole[i] = '<span style="background-color: brown; color:white;">' + parole[i] + "</span>";
             } else {
-              punt = parole[i].substring(parole[i].indexOf(parola) + parola.length);
-              parole[i] = '<span style="background-color: brown; color:white;">' + parola + "</span>" + punt;
+              punt = parole[i].substring(parole[i].indexOf(world) + world.length);
+              parole[i] = '<span style="background-color: brown; color:white;">' + world + "</span>" + punt;
             }
             document.getElementById('txtBacon').innerHTML = parole.join(' ');
           }
@@ -40,7 +34,7 @@ btCerca.addEventListener('click', function(e){
       }
     }else{
       document.getElementById('txtBacon').innerHTML = txt;
-      document.getElementById('write').innerHTML = "Parola " + parola + " non è stata trovata.";
+      document.getElementById('write').textContent = "Parola " + world + " non è stata trovata.";
     }
     document.getElementById('txtcerca').value = "";
   }
@@ -49,3 +43,14 @@ btCerca.addEventListener('click', function(e){
     document.getElementById('write').innerHTML = "Immettere parola da cercare!";
   }
 });
+
+function stampaConPunt(parole, world)
+{
+  for(let i=0; i<parole.length; i++)
+  {
+      if(parole[i].toLowerCase() == world){
+        parole[i] = '<span style="background-color: brown; color:white;">' + world + "</span>";
+      }
+      document.getElementById('txtBacon').innerHTML = parole.join(' ');
+  }
+}
