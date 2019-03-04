de3813ccde3a09b1733e33dc578de709271d43ee
Flavio Cherchi <flavio.cherchi@gmail.com>
2019-03-04 20:27:50 +0100

    Memory game - Aeternal damnation

--- js/memory.js
+++ js/memory.js
@@ -1,49 +1,58 @@
 var gabbia = [
   "Topo",
-  "Topo",
-  "Bue",
   "Bue",
   "Tigre",
-  "Tigre",
-  "Coniglio",
   "Coniglio",
   "Drago",
-  "Drago",
   "Serpente",
-  "Serpente",
-  "Cavallo",
   "Cavallo",
   "Capra",
-  "Capra"
 ];
+
+var coppie = [];
+
+function coupling(){
+var a1;
+var a2;
+for (let i = 0; i < gabbia.length; i++){
+
+   a1 =  "A_" + gabbia[i];
+   a2 =  "B_" + gabbia[i];
+   coppie.unshift(a1,a2);
+  }
+}
+coupling();
 
 function shuffle() {
   var j, x, i;
-  for (i = gabbia.length - 1; i > 0; i--) {
+  for (i = coppie.length - 1; i > 0; i--) {
     j = Math.floor(Math.random() * (i + 1));
-    x = gabbia[i];
-    gabbia[i] = gabbia[j];
-    gabbia[j] = x;
+    x = coppie[i];
+    coppie[i] = coppie[j];
+    coppie[j] = x;
   }
 
-  return gabbia;
+  return coppie;
 }
 shuffle();
 
 function memoria() {
   var a = 0;
-  for (i = 0; i < gabbia.length; i++) {
+  var link = "";
+  for (i = 0; i < coppie.length; i++) {
     a++;
+    link = coppie[i].substring(2);
     var icon = "icon" + a;
-    var final = "<img src=img/zoo/" + gabbia[i] + '.jpg class="icon">';
+    var final = "<img src=img/zoo/" + link + ".jpg onclick=\"mirror(this.id)\" class=\"icon\" id=\"" + coppie[i] + "\">";
     document.getElementById(icon).innerHTML = final;
-  }
+    console.log(final);
+    }
 }
 memoria();
 
 function restart() {
   clearInterval(displayMemory);
-  timer = 60;
+  timer = 61;
   displayMemory();
   shuffle();
   document.getElementById("nuova").innerHTML = memoria();
@@ -58,8 +67,38 @@ function displayMemory() {
 }
 notemp = setInterval(displayMemory, 1000);
 
-function mirror() {
-  if (history.go(0) === history.go(-1)) {
-    console.log("corretto");
-  } else console.log("sbagliato");
+
+function opacity(){
+  var gigi = document.getElementsByClassName("icon"); 
+  for (let i = 0; i < gigi.length; i++){
+  gigi[i].style.opacity = "0.5";  //dev'essere a zero per funzionare correttamente
+  }
+}
+opacity();
+
+var specchio =[];
+function mirror(id) {
+  var op = document.getElementById(id); 
+  op.style.opacity = "1";
+  specchio.unshift(id);
+  if (specchio[0].substring(2) != null){
+      
+    if(specchio[0].substring(2) === specchio[1].substring(2)){
+          console.log("un punto!!!");
+          specchio.shift();
+          specchio.shift();
+          console.log(specchio);
+      }
+  
+      else {
+          console.log("balengo!");
+          specchio.shift();
+          specchio.shift();
+          
+      }
+
+  specchio.unshift(id);
+    console.log(specchio);
+
+}
 }
