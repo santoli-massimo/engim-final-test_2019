d68236c7261c141b7c7eb96c2335bb343220c52d
Flavio Cherchi <flavio.cherchi@gmail.com>
2019-03-04 15:04:40 +0100

    Memory game - countdown implemented

--- js/memory.js
+++ js/memory.js
@@ -1,40 +1,75 @@
 var gabbia = [
   "Topo",
+  "Topo",
+  "Bue",
   "Bue",
   "Tigre",
+  "Tigre",
+  "Coniglio",
   "Coniglio",
   "Drago",
+  "Drago",
+  "Serpente",
   "Serpente",
   "Cavallo",
+  "Cavallo",
   "Capra",
-  "Scimmia",
-  "Gallo",
-  "Cane",
-  "Maiale"
+  "Capra"
 ];
 
+function shuffle() {
+  var j, x, i;
+  for (i = gabbia.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = gabbia[i];
+    gabbia[i] = gabbia[j];
+    gabbia[j] = x;
+  }
+
+  return gabbia;
+}
+shuffle();
+
 function memoria() {
-
   var a = 0;
-  for (i = 0; i < 16; i++) {
+  for (i = 0; i < gabbia.length; i++) {
     a++;
-    
-    var res = gabbia[Math.floor(Math.random() * 12)];
-
     var icon = "icon" + a;
-    var res1 =
-      "<img src=img/zoo/" +
-      gabbia[Math.floor(Math.random() * 8)] +
-      '.jpg class="icon">';
-    var res2 = res1;
-    console.log("res1: "+res1);
-    console.log("res2: "+res2);
-    document.getElementById(icon).innerHTML = res1;
-    document.getElementById(icon).innerHTML = res2;
+    var final = "<img src=img/zoo/" + gabbia[i] + '.jpg class="icon">';
+    document.getElementById(icon).innerHTML = final;
   }
 }
 memoria();
 
 function restart() {
+  clearInterval(displayMemory);
+  timer = 61;
+  displayMemory();
+  shuffle();
   document.getElementById("nuova").innerHTML = memoria();
+}
+
+
+function displayMemory() {
+  timer -= 1 ;
+  timer = timer < 10 ? "0" + timer : timer;
+  if (timer <= 0) {
+    document.getElementById("displayMemory").innerHTML = "fine";
+    clearInterval(displayMemory);
+  }
+  else
+  document.getElementById("displayMemory").innerHTML = timer;
+}
+notemp = setInterval(displayMemory, 1000);
+
+
+
+
+
+
+
+function mirror() {
+  if (history.go(0) === history.go(-1)) {
+    console.log("corretto");
+  } else console.log("sbagliato");
 }
