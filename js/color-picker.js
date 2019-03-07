d67c50cdbf15bae92a1615395bc1fb196adb7286
ciprian <ciprian.giurgica@gmail.com>
2019-03-07 12:46:39 +0100

    Color Picker

---
+++ js/color-picker.js
@@ -0,0 +1,11 @@
+function changeTitleColor(){
+    document.getElementById("title").style.color = document.getElementById("color-title").value
+}
+
+function changeTextColor(){
+    document.getElementById("text").style.color = document.getElementById("color-text").value
+}
+
+function changeBackgroundColor(){
+    document.getElementById("background").style.backgroundColor = document.getElementById("color-background").value
+}
